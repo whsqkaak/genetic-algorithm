de77c3bf980e2fc01001b990f921337d2245e5af
JmirY <yongseokj3@naver.com>
2016-10-14 12:56:58 +0900

    Change Measure.py
    improt문 변경
    measure 개체에 chord 필드 추가

--- MidiToScore/src/Chords.py
+++ MidiToScore/src/Chords.py
@@ -1,4 +1,4 @@
-from Notes import Note
+from src.Notes import *
 
 
 class Chord(object):
--- MidiToScore/src/Measures.py
+++ MidiToScore/src/Measures.py
@@ -2,6 +2,8 @@ class Measure(object):
     def __init__(self):
         self.sum_of_duration = 0
         self.measure = []
+        # 유전 알고리즘으로 생성한 코드
+        self.chord
 
     def add_note(self, note):
         self.measure.append(note)
