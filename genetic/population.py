3a5f41c66826c9cee264f4c50443ea139e5d9fa8
JmirY <yongseokj3@naver.com>
2016-10-23 21:41:25 +0900

    [변경] 코드 수정

--- genetic/population.py
+++ genetic/population.py
@@ -5,13 +5,14 @@ import random
 class Population(object):
 	def __init__(self, scorewriter):
 		self.scorewriter = scorewriter  # 생성 시 ScoreWriter 객체를 필드로 받아옴
-		n = 100  # 개체군의 크기
+		self.n = 100  # 개체군의 크기
 		
 	def population(self):
 		# 초기 해집단 생성하는 함수
 		n = self.n
 		s = self.scorewriter.score
-		pop = [n][len(s) * 2]  # 개체군
+		# pop = [n][len(s) * 2]  # 개체군  # org
+		pop = [[0 for col in range(len(s))] for row in range(n)]  # 개체군
 		for i in range(n):
 			for j in range(len(s)):
 				c = random.choice(self.scorewriter.diatonic_chords)  # 임의의 다이아토닉 코드
@@ -36,7 +37,8 @@ class Population(object):
 	def len_nonharmonic(self, chord, measure):
 		# 후미 반 마디에서 chord 에 대한 비화성음들의 길이의 합을 구하는 함수
 		m = measure.measure
-		m = m[:(len(m) / 2)]  # 마디를 반으로 나눈다
+		# m = m[:(len(m) / 2)]  # 마디를 반으로 나눈다   # org
+		m = m[:(len(m) // 2)]  # 마디를 반으로 나눈다
 		length = 0
 		for note in m:
 			if self.is_harmonic(chord, note):
